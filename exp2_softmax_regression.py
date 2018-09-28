7aa4f88e598c841627f4565b4c75b8a0ad02423f
Akari Ishikawa <ra163282@martins.lab.ic.unicamp.br>
2018-09-27 22:18:53 -0300

    Creating softmax regression

--- exp2_softmax_regression.py
+++ exp2_softmax_regression.py
@@ -8,31 +8,21 @@ import get_dataset
 
 
 ##
-## REGRESSAO LOGISTICA COM REDE NEURAL DE UMA CAMADA: ONE VS. ALL
+## REGRESSAO SOFTMAX COM REDE NEURAL DE UMA CAMADA
 ##    
 
-def onevsall(classe, train_set, train_labels, valid_set, valid_labels):
+def main():
+    train_set, valid_set, train_labels, valid_labels = get_dataset()
 
-    neural_net_logistic = []
-    y_train = np.zeros((train_labels.shape))
-    y_train[train_labels == classe] = 1
-    y_train[train_labels != classe] = 0
-    y_valid = np.zeros((valid_labels.shape))
-    y_valid [valid_labels == classe] = 1
-    y_valid [valid_labels != classe] = 0
+    neural_softmax = NeuralNetwork()
 
     # Adicionando a camada de input no indice 0
-    neural_net_logistic.append(Layer(False, train_set.shape[1], train_set.shape[1]))
-    neural_net_logistic[0].forward(train_set,identidade)
+    neural_softmax.camadas.append(Layer(False, train_set.shape[1], train_set.shape[1]))
+    neural_softmax.camadas[0].forward(train_set,identidade)
 
     # Adicionando a camada de saída no índice 1
-    neural_net_logistic.append(Layer(True, neural_net_logistic[0].activation.shape[1], 1 ))
-
-    train(neural_net_logistic, train_set, y_train, 0.02, 1000)
-    predict(neural_net_logistic, valid_set, y_valid)
-    
-def main():
-    print("oi")
+    neural_softmax.camadas.append(Layer(True, neural_softmax.camadas[0].activation.shape[1], 1 ))
+    print("Segunda camada adicionada")
 
 
 if __name__ == "__main__":
