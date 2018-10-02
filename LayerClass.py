7c72e7e9b5d4a759a8e74570ad504a7dea369ee2
Akari Ishikawa <ra163282@rabugento.lab.ic.unicamp.br>
2018-10-02 15:09:17 -0300

    Working in exp2

--- LayerClass.py
+++ LayerClass.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functions import *
 
 class Layer:
     def __init__(self, random, input_size, output_size):
@@ -19,30 +20,27 @@ class NeuralNetwork:
         self.functions = []
         self.derivatives = []
 
-    def loss(self,Y):
-        H = self.activation
+    def loss(self,H,Y):
         return np.mean(np.subtract(np.multiply(np.multiply(-1, Y),np.log(H)),np.multiply(np.subtract(1, Y),np.log(np.subtract(1,H)))))
 
     def forward(self,X):
         out = len(self.camadas)-1
         inp = 0
         self.camadas[0].activation = X
-        for i in range(0,len(self.camadas)):
-            self.camadas[i].activation = self.functions[i](self.camadas[i-1].activation.dot(self.camadas[i].weights))+self.camadas[i].bias
+        for i in range(1,len(self.camadas)):
+            self.camadas[i].activation = np.add(self.functions[i](self.camadas[i-1].activation.dot(self.camadas[i].weights)),self.camadas[i].bias.T)
         return self.camadas[out].activation
-
 
     def backward(self,  X, y, learning_rate):
         out = len(self.camadas)-1
         inp = 0
-        self.camadas[out].error = np.subtract(y,self.camadas[out].activation)
-        self.camadas[out].delta = self.camadas[out].error*self.derivatives[out](self.camadas[out].activation)
-        
-        for i in range(len(camadas)-2,0,-1):
+        self.camadas[out].error = cross_entropy(self.camadas[out].activation,y)
+        self.camadas[out].delta = self.camadas[out].error*self.derivatives[out](self.camadas[out].activation, y)
+        for i in range(len(self.camadas)-2,0,-1):
             print(i)
             self.camadas[i].error = self.camadas[i+1].delta.dot(self.camadas[i].weights)
             self.camadas[i].delta = self.camadas[i].error*self.derivatives[i](self.camadas[i].activation)
-        for i in range(len(camadas)-1,0,-1):
+        for i in range(len(self.camadas)-1,0,-1):
             self.camadas[i].weights += learning_rate*self.camadas[i-1].activation.T.dot(self.camadas[i].delta)
 
     def predict(self,X,y):
@@ -52,7 +50,6 @@ class NeuralNetwork:
         camadas[0].activation = X
         for i in range(1,len(camadas)):
             camadas[i].activation = sigmoid(camadas[i-1].activation.dot(camadas[i].weights))
-
         output = camadas[out].activation
         preds = camadas[out].activation
         preds[preds > 0.5] = 1
@@ -62,14 +59,15 @@ class NeuralNetwork:
 
     def train(self,X,y,learning_rate,iteracoes, printacc):
         for i in range(0, iteracoes):
-            self.forward(X)
-            self.backward(X,y,learning_rate)
+            output = self.forward(X)
             if printacc:
-                preds = np.copy(self.camadas[1].activation)
+                preds = np.copy(output)
+                print("Loss: "+str(cross_entropy(output, y)))
                 preds[preds > 0.5] = 1
                 preds[preds <=0.5] = 0
                 acc = sum(preds == y)
                 print("Acc: "+str(acc/len(y)))
+            self.backward(X,y,learning_rate)
 
     def predict_prob(self,X,y):
         camadas = np.copy(self.camadas)
